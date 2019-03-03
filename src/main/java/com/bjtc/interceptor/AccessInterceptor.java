8cfeb884b602ce8c78b2ef7b8529a06f47342773
fengpeiyao <479501539@qq.com>
2019-03-03 11:04:20 +0800

    用户模块完成

--- src/main/java/com/bjtc/interceptor/AccessInterceptor.java
+++ src/main/java/com/bjtc/interceptor/AccessInterceptor.java
@@ -1,13 +1,19 @@
 package com.bjtc.interceptor;
 
+import com.bjtc.pojo.User;
+import com.bjtc.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AccessInterceptor implements HandlerInterceptor {
-
+    @Autowired
+    private UserService userService;
     @Override
     public void afterCompletion(HttpServletRequest arg0, HttpServletResponse response, Object object, Exception arg3)
             throws Exception {
@@ -22,6 +28,35 @@ public class AccessInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object object) throws Exception {
+        if(request.getRequestURI().equals("/userLogin")){
+            response.setHeader("message","success");
+            return true;
+        }
+        boolean isExistUser=false;
+        response.setHeader("message","false");
+        HttpSession session = request.getSession(false);
+        if(session==null){
+            response.setHeader("message","NoSession");
+            response.sendRedirect("index.jsp");
+            return false;
+        }
+        Cookie[] cookies = request.getCookies();
+        Object userPhone = session.getAttribute("userPhone");
+        Object userPassword = session.getAttribute("userPassword");
+        for(Cookie cookie:cookies){
+            if(cookie.getName().equals("userPhone")){
+                isExistUser=true;
+                User user = userService.selectUserByPhoneNumber(userPhone.toString());
+                if(!cookie.getValue().equals(userPhone)||userPassword.equals(user.getUserPassword())){
+                    response.sendRedirect("index.jsp");
+                    return false;
+                }
+            }
+        }
+        if(!isExistUser){
+            response.sendRedirect("index.jsp");
+            return false;
+        }
         // 设置：Access-Control-Allow-Origin头，处理Session问题
         response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
         response.setHeader("Access-Control-Allow-Credentials", "true");
@@ -31,6 +66,7 @@ public class AccessInterceptor implements HandlerInterceptor {
             response.addHeader("Access-Control-Allow-Headers", "Content-Type,Origin,Accept");
             response.addHeader("Access-Control-Max-Age", "120");
         }
+        response.setHeader("message","success");
         return true;
     }
 }
